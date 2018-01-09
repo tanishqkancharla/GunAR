7ab12f58b8ca232ba55c70960cc9e3b812e48210
Tanishq Kancharla <Tanishq@tanishqs-macbook-pro.local>
2018-01-09 08:36:57 -0500

    Initial Commit

---
+++ GunAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GunAR
+//
+//  Created by Tanishq Kancharla on 1/9/18.
+//  Copyright © 2018 Tanishq Kancharla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
