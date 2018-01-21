7bb706014b8249c0007f4a7a70dcec68d55608ae
Tanishq Kancharla <Tanishq@Tanishqs-MBP.wv.cc.cmu.edu>
2018-01-21 12:34:22 -0500

    Updated GunAR with single player

---
+++ GPIOcharacteristic.js
@@ -0,0 +1,75 @@
+var util = require('util');
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
+var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
+var bleno = require('bleno');
+//install onoff through npm
+//connect GPIO3 to button then to ground
+var BlenoCharacteristic = bleno.Characteristic;
+
+var EchoCharacteristic = function() {
+  EchoCharacteristic.super_.call(this, {
+    uuid: 'fh78',
+    properties: ['write', 'notify'],
+    value: null
+  });
+
+  this._value = new Buffer(0);
+  this._updateValueCallback = null;
+};
+
+util.inherits(EchoCharacteristic, BlenoCharacteristic);
+
+EchoCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+  this._value = data;
+
+  console.log('EchoCharacteristic - onWriteRequest: value = ' + this._value.toString('hex'));
+
+  if (this._updateValueCallback) {
+    console.log('EchoCharacteristic - onWriteRequest: notifying');
+
+    this._updateValueCallback(this._value);
+  }
+
+  callback(this.RESULT_SUCCESS);
+};
+
+
+
+pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
+  if (err) { //if an error
+    console.error('There was an error', err); //output error message to console
+  return;
+  }
+  LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
+  EchoCharacteristic.prototype.update(value)
+});
+
+function unexportOnClose() { //function to run when exiting program
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport LED GPIO to free resources
+  pushButton.unexport(); // Unexport Button GPIO to free resources
+};
+
+process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
+
+EchoCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+  console.log('EchoCharacteristic - onSubscribe');
+
+  this._updateValueCallback = updateValueCallback;
+};
+
+EchoCharacteristic.prototype.onUnsubscribe = function() {
+  console.log('EchoCharacteristic - onUnsubscribe');
+
+  this._updateValueCallback = null;
+};
+
+EchoCharacteristic.prototype.update = function(value){
+  this._value = value;
+  if (this._updateValueCallback) {
+        this._updateValueCallback(this._value);
+    }
+}
+
+module.exports = EchoCharacteristic;
